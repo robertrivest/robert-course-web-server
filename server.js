ff2e9c7cbb888f73f7b758fec1250db058b2460e
Robert Rivest <robertrivest68@gmail.com>
2017-11-26 18:19:37 -0500

    Add Projects Page

--- server.js
+++ server.js
@@ -39,6 +39,13 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        welcomeMessage: 'Welcome to the Projects Area'
+    });
+});
+
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
